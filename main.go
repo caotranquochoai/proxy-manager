c3d171564626517141a99545b93916a153ff08e7
caotranquochoai <54810821+caotranquochoai@users.noreply.github.com>
2024-03-08 01:14:50 +0700

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"path/filepath"
 
-	"github.com/hidu/proxy-manager/internal"
+	"github.com/caotranquochoai/proxy-manager/internal"
 )
 
 var configPath = flag.String("conf", "./conf/proxy.toml", "proxy's config file")
@@ -15,7 +15,7 @@ var initConf = flag.Bool("init", false, "create config files if not exists")
 func init() {
 	flag.Usage = func() {
 		fmt.Println("proxy manager\n  version:", internal.GetVersion())
-		fmt.Print("  https://github.com/hidu/proxy-manager/\n\n")
+		fmt.Print("  https://github.com/caotranquochoai/proxy-manager/\n\n")
 		flag.PrintDefaults()
 	}
 	log.SetFlags(log.Lshortfile | log.LstdFlags | log.Ldate)
